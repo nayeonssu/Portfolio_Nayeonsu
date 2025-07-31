07f4ec31c4c164a15642d5cf546f9a758d1d92e2
nayeonssu <sys7552@gmail.com>
2025-08-01 02:50:06 +0900

    디자인 영역 추가

--- scripts/index.js
+++ scripts/index.js
@@ -11,6 +11,23 @@ const project1 = new Swiper('#project_swiper',{
     loop:true,/* 무한반복 */
 })
 
+//디자인 슬라이즈 가로배너
+const designTransverse = new Swiper('#transverse_swiper',{
+    slidesPerView:2.5,
+    autoplay:{delay:1,},
+    spaceBetween:20,
+    speed:4000,
+    loop:true,
+})
+//디자인 슬라이즈 세로배너
+const designLength = new Swiper('#length_swiper',{
+    slidesPerView:3.5,
+    autoplay:{delay:0,},
+    spaceBetween:20,
+    speed:5000,
+    loop:true,
+})
+
 //내비게이션 클릭 시 해당 위치 수직 스와이프(wrap변수)로 이동
 const nav = document.querySelectorAll('nav a');
 //수직 스와이프 이동 함수
@@ -22,3 +39,21 @@ nav.forEach((obj,idx)=>{
         wrap.slideTo(idx, 1000);/* obj클릭했을때 1초동안 그 위치로 이동한다. */
     })
 })
+
+//배너 클릭 시 팝업 실행(클릭한 이미지가 팝업 이미지로 교체)
+const lengthProject = document.querySelectorAll('#length_swiper .swiper-slide')/* 세로배너 4개의 스와이퍼가 모두 잡힘 */
+const popup = document.querySelector('.popup_bg')/* 팝업실행할때 */
+console.log(lengthProject,popup)
+
+for(let banner of lengthProject){
+    banner.addEventListener('click',()=>{
+        popup.style.display = 'block'/* popup의 style의 display를 보이게 하겠다 */ /* css에서 안보이게 설정해놨엤다. */
+        popup.children[0].children[0].src = banner.children[0].src/* css -> popup_bg .popup img 자식을 잡고 싶은거라면 childern을 써야한다. */
+        wrap.mousewheel.disable();//disable()은 막기 enable()은 풀기
+    })
+}
+//팝업창 닫을때
+popup.addEventListener('click',()=>{
+    popup.style.display = 'none'
+    wrap.mousewheel.enable()//스크롤 기능 풀기
+})/* 팝업을 실행했을 때 아무곳이나 누르면 닫힌다. */
